6b60de35837afe1874e58e344276a7c5ba9e7949
Allen <allenliu0476@gmail.com>
2025-04-30 17:17:28 -0400

    updated saveUser server action

--- frontend/app/actions/users/saveUser.ts
+++ frontend/app/actions/users/saveUser.ts
@@ -1,21 +1,25 @@
-"use server"
 import { prisma } from '@/utils/prisma/client';
-import { User } from '@/app/types/user';
 
-export default async function saveUser(userData: User) {
+interface UserData {
+    firstName: string;
+    lastName: string;
+    phone: string;
+    email: string;
+    clerkId: string;
+    hobbies: string[]; // Array of hobby names
+}
+
+/**
+ * Saves a user to the database.
+ * @param {UserData} userData - The user data to save.
+ * @returns {Promise<User>} The created user object.
+ * @throws {Error} If there is an error creating the user.
+ */
+
+export async function saveUser(userData: UserData) {
 
     try {
-
-        const existingUser = await prisma.user.findUnique({
-            where: {
-                clerkId: userData.clerkId,
-            },
-        });
-        if (existingUser) {
-            console.log('User already exists:', existingUser);
-            return existingUser;
-        }
-        const newUser = await prisma.user.create({
+        const user = await prisma.user.create({
             data: {
                 clerkId: userData.clerkId,
                 email: userData.email,
@@ -23,18 +27,17 @@ export default async function saveUser(userData: User) {
                 lastName: userData.lastName,
                 phone: userData.phone,
                 hobbies: {
-                    connectOrCreate: userData.hobbies.map(hobby => ({
-                        where: { name: hobby },
-                        create: { name: hobby },
+                    connectOrCreate: userData.hobbies.map((hobby) => ({
+                        where: { name: hobby },  // Check if the hobby exists by name
+                        create: { name: hobby }, // If not, create it
                     })),
                 },
             },
         });
-        console.log('New user created:', newUser);
-        return newUser;
 
+        return user;
     } catch (error) {
-        console.error('Error saving user:', error);
+        console.error('Error creating user:', error);
         throw error;
     }
 }
